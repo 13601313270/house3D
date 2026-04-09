36a8a695448051d5f95478296f34eb068d57deb4
wanghaoran <309568486@qq.com>
2026-04-09 17:27:29 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  ignorePatterns: [
+    'node_modules/**',
+    'public/**',
+    'src/plugin/**'
+  ],
+  rules: {
+    'space-before-function-paren': 'off',
+    'eol-last': 'off',
+    'no-trailing-spaces': 'off',
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'no-multi-spaces': 'off',
+    'vue/multi-word-component-names': 'off',
+    'brace-style': 'off',
+    quotes: 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    'no-async-promise-executor': 'off',
+    'operator-linebreak': 'off',
+    'vue/require-v-for-key': 'off',
+    'vue/valid-v-for': 'off',
+    'prefer-rest-params': 'off',
+    'no-empty': 'off',
+    'no-useless-return': 'off',
+    'prefer-promise-reject-errors': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'quote-props': 'off'
+  },
+  env: {
+    'vue/setup-compiler-macros': true
+  }
+}
---
+++ src/views/addPeopleToScene.ts
@@ -0,0 +1,59 @@
+import * as THREE from 'three'
+// @ts-ignore
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+// @ts-ignore
+import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
+
+const objLoader = new OBJLoader();
+const mtlLoader = new MTLLoader();
+async function addPeopleToScene(
+  scene: THREE.Scene,
+  type: 'people' | 'peopleSit',
+  x: number,
+  y: number,
+  z: number,
+  rotationY: number = 0
+) {
+  const mtlLoader = new MTLLoader();
+  mtlLoader.load(
+    type === 'people' ? './people/1.mtl' : './peoplesit/1.mtl', // 你的材质文件路径，通常和 .obj 放在同一目录
+    (materials: any) => {
+      // 关键步骤：预加载所有材质，让纹理图片准备好
+      materials.preload();
+
+      // 3. 将加载好的材质集传给 OBJLoader
+      objLoader.setMaterials(materials);
+
+      objLoader.load(
+        type === 'people' ? './people/1.obj' : './peoplesit/1.obj',
+        function (object: any) {
+          // object.scale.set(0.3, 0.3, 0.3); // FBX通常很大
+          object.position.set(x, y, z);
+          object.rotation.y = rotationY;
+          scene.add(object);
+        }
+      );
+      // // 4. 加载 OBJ 模型文件
+      // objLoader.load(
+      //     'path/to/your/model.obj',
+      //     (object) => {
+      //         // 模型已经自动应用了材质，直接添加到场景即可
+      //         scene.add(object);
+      //     },
+      //     (xhr) => {
+      //         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+      //     },
+      //     (error) => {
+      //         console.error('An error happened while loading the OBJ file', error);
+      //     }
+      // );
+    },
+    (xhr: any) => {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    (error: any) => {
+      console.error('An error happened while loading the MTL file', error);
+    }
+  );
+}
+export default addPeopleToScene
