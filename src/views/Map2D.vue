9afd3e13f227fe40f3b6c03158c51a5ea16e32d2
wanghaoran <309568486@qq.com>
2026-04-24 11:08:21 +0800

    update

--- src/views/Map2D.vue
+++ src/views/Map2D.vue
@@ -53,21 +53,21 @@
                 <input v-else-if="item.dataType === 'boolean'" type="checkbox" :checked="editPropInputInfo[item.id]"
                   @change="updateEditPropInputInfoBoolean(item.id, $event)" />
                 <div v-else-if="item.dataType === 'material'" class="materialList">
-                  <div class="materialItem" :class="{ active: editPropInputInfo[item.id] === 0 }"
-                    @click="updateEditPropInputNumberInfo(item.id, 0)">
-                    <div class="imgOuting">
-                      <img src="../assets/Empty.png" alt="noMaterial" class="img"
-                        style="width: 50px;background-color: white;" />
+                  <div @click="allMaterialShow = true, allMaterialShowPropId = item.id">
+                    <div class="materialItem" v-if="editPropInputInfo[item.id] === 0">
+                      <div class="imgOuting">
+                        <img src="../assets/Empty.png" alt="noMaterial" class="img"
+                          style="width: 50px;background-color: white;" />
+                      </div>
+                      <div class="name">无</div>
                     </div>
-                    <div class="name">无</div>
-                  </div>
-                  <div v-for="item2 in allMaterial" :key="item2.id" class="materialItem"
-                    :class="{ active: editPropInputInfo[item.id] === item2.id }"
-                    @click="updateEditPropInputNumberInfo(item.id, item2.id)">
-                    <div class="imgOuting">
-                      <img :src="item2.img" alt="material" class="img" />
+                    <div v-for="item2 in allMaterial.filter(item2 => editPropInputInfo[item.id] === item2.id)"
+                      :key="item2.id" class="materialItem">
+                      <div class="imgOuting">
+                        <img :src="item2.img" alt="material" class="img" />
+                      </div>
+                      <div class="name">{{ item2.name }}</div>
                     </div>
-                    <div class="name">{{ item2.name }}</div>
                   </div>
                 </div>
               </div>
@@ -91,6 +91,31 @@
       <!-- {{ cameraState2 }} -->
       <Canvas3D v-if="cameraState2" ref="canvas3DRef2" :world="worldApi" :cameraState="cameraState2"
         :aspectRatio="cameraState2.aspectW / cameraState2.aspectH" />
+    </div>
+
+    <div class="allMaterialPanel" v-if="allMaterialShow && allMaterialShowPropId"
+      @click.self="allMaterialShow = false, allMaterialShowPropId = undefined">
+      <div class="allMaterialPanelInner">
+        <div class="title">所有材质</div>
+        <div class="list">
+          <div class="materialItem"
+            @click="updateEditPropInputNumberInfo(allMaterialShowPropId, 0), allMaterialShow = false">
+            <div class="imgOuting">
+              <img src="../assets/Empty.png" alt="noMaterial" class="img"
+                style="width: 50px;background-color: white;" />
+            </div>
+            <div class="name">无</div>
+          </div>
+          <div v-for="item2 in allMaterial" :key="item2.id" class="materialItem"
+            :class="{ active: editPropInputInfo[item2.id] === item2.id }"
+            @click="updateEditPropInputNumberInfo(allMaterialShowPropId, item2.id), allMaterialShow = false">
+            <div class="imgOuting">
+              <img :src="item2.img" alt="material" class="img" />
+            </div>
+            <div class="name">{{ item2.name }}</div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -141,6 +166,9 @@ const isSplitting = ref(false)
 const canvasSize = ref({ width: 0, height: 0 })
 const zoomLevel = ref(1)
 const wallThickness = ref<number>(20)
+
+const allMaterialShow = ref(false)
+const allMaterialShowPropId = ref<string>()
 
 const cameraState = ref<CameraState>({
   targetPositionX: 0,
@@ -1397,7 +1425,7 @@ watch(() => editPropInputInfo.value, () => {
 })
 
 function updateEditPropInputNumberInfo(id: string, event: Event | number) {
-  if (event instanceof Event) {
+  if (event instanceof Event && id) {
     // @ts-ignore
     editPropInputInfo.value[id] = +(+event.target.value)
   } else {
@@ -1564,7 +1592,6 @@ function updateEditPropInputInfoBoolean(id: string, event: Event) {
 .materialList {
   display: flex;
   flex-wrap: wrap;
-  width: 200px;
 
   .materialItem {
     width: 50px;
@@ -1577,6 +1604,7 @@ function updateEditPropInputInfoBoolean(id: string, event: Event) {
 
     &:hover {
       transform: scale(2);
+      transform-origin: right center;
       transition: all 0.1s;
     }
 
@@ -1591,6 +1619,9 @@ function updateEditPropInputInfoBoolean(id: string, event: Event) {
       overflow: hidden;
       position: relative;
       z-index: -1;
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
       .img {
         width: 300px; // 稍微放大一点，不然50像素，看不清细节
@@ -1602,6 +1633,81 @@ function updateEditPropInputInfoBoolean(id: string, event: Event) {
       font-size: 14px;
       text-align: center;
       background-color: white;
+    }
+  }
+}
+
+.allMaterialPanel {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 1001;
+  background-color: #00000075;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .allMaterialPanelInner {
+    display: flex;
+    width: 610px;
+    padding: 8px;
+    min-height: 200px;
+    max-height: 70vh;
+    background-color: white;
+    border-radius: 8px;
+    overflow: auto;
+    flex-direction: column;
+    align-items: center;
+
+    .title {
+      font-size: 24px;
+    }
+
+    .list {
+      width: 593px;
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      gap: 8px;
+      margin-top: 8px;
+
+      .materialItem {
+        flex-shrink: 0;
+        border: solid 1px #b2b2b2;
+        display: flex;
+        flex-direction: column;
+        box-sizing: border-box;
+        border-radius: 4px;
+        overflow: hidden;
+
+        &.active {
+          border: solid 1px #1890ff;
+          box-shadow: inset 0 0 0px 2px #1890ff;
+        }
+
+        .imgOuting {
+          width: 140px;
+          height: 140px;
+          overflow: hidden;
+          position: relative;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          .img {
+            width: 140px;
+          }
+        }
+
+        .name {
+          font-size: 14px;
+          text-align: center;
+          background-color: white;
+        }
+      }
     }
   }
 }
