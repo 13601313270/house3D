2eea9be323a66f9b2f29b345b33faba71d91b40a
wanghaoran <309568486@qq.com>
2026-05-01 14:17:19 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,11 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Map2D from '../views/Map2D.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: import(/* webpackChunkName: "map2d" */ '../views/Map2D.vue')
-  },
-  {
-    path: '/edit',
-    name: 'edit',
-    component: () => import(/* webpackChunkName: "edit" */ '../views/EditView.vue')
-  },
-  {
-    path: '/map2d',
-    name: 'map2d',
-    component: () => import(/* webpackChunkName: "map2d" */ '../views/Map2D.vue')
+    component: Map2D
   },
 ]
 
