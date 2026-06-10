b3e7cd882b27506c419addb02b8624a411890842
wanghaoran <309568486@qq.com>
2026-06-10 13:12:39 +0800

    update

--- src/entities/curtain/entity.ts
+++ src/entities/curtain/entity.ts
@@ -9,7 +9,7 @@ import { MatchRectArea } from '@/utils/matchArea';
 import { importImgFileHead } from '../allObjs';
 
 export class CurtainEntity extends EntityClass<CurtainData> {
-  name: string = '方形幕布'
+  name: string = '垂直方形幕布'
   type: string = 'curtain'
   isPointObj: boolean = true
   private circleRadius = 6
--- src/entities/curtain/index.ts
+++ src/entities/curtain/index.ts
@@ -2,7 +2,7 @@ import { CurtainEntity } from "./entity"
 import PluginType from "../pluginType"
 
 const CurtainPlugin: PluginType = {
-  name: '方形幕布',
+  name: '垂直方形幕布',
   key: 'curtain',
   type: 'curtain',
   entity: CurtainEntity,
