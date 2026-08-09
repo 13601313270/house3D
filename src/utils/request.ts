e99bcaeefffe7d626f95565c1fcf6941021c761c
wanghaoran <309568486@qq.com>
2026-08-09 16:49:32 +0800

    update

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 // 创建一个 axios 实例
 const service: AxiosInstance = axios.create({
   baseURL: location.host === 'localhost:8080' ? ('http://' + location.host + '/api') : 'https://api.studying1v1.com', // 所有的请求地址前缀部分
-  timeout: 60000, // 请求超时时间毫秒
+  timeout: 240000, // 请求超时时间毫秒
   // withCredentials: true, // 异步请求携带cookie
   headers: {
     // 设置后端需要的传参类型
